fbc657a23af230cce2a9cdbcc35fbd9c7b1e5960
PStar-7 <juniarda28@gmail.com>
2023-09-19 11:38:35 +0700

    first init

---
+++ challenge7644.js
@@ -0,0 +1,12 @@
+function seriesUpTerms(n) {
+  let total = 0;
+  let arr = [];
+
+  for (let i = 0; i < n; i++) {
+    total += 10 ** i * 2 + 0;
+    arr.push(total);
+  }
+  const result = arr.reduce((a, b) => a + b);
+  console.log(result);
+}
+seriesUpTerms(5);
