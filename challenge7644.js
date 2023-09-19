b92c580215e1846481200b7bad3d91a895c31ac8
PStar-7 <juniarda28@gmail.com>
2023-09-19 11:51:53 +0700

    angka 0 dihapus

--- challenge7644.js
+++ challenge7644.js
@@ -3,7 +3,7 @@ function seriesUpTerms(n) {
   let arr = [];
 
   for (let i = 0; i < n; i++) {
-    total += 10 ** i * 2 + 0;
+    total += 10 ** i * 2;
     arr.push(total);
   }
   const result = arr.reduce((a, b) => a + b);
